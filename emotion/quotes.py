84f2557d4e60c369808958f7547882f880e5845a
Amritavarshini R V <amritavarshinirv@gmail.com>
2025-12-28 19:07:54 +0530

    Add files via upload

---
+++ emotion/quotes.py
@@ -0,0 +1,46 @@
+quotes = {
+    "happy": [
+        "Happiness is not by chance, but by choice.",
+        "Smile – it increases your face value.",
+        "Joy is the simplest form of gratitude.",
+        "Your happiness inspires others."
+    ],
+
+    "sad": [
+        "Every storm runs out of rain.",
+        "You are stronger than you think.",
+        "Pain is temporary, strength is permanent.",
+        "This moment will pass."
+    ],
+
+    "angry": [
+        "Peace begins when expectations end.",
+        "Control your anger before it controls you.",
+        "Silence is sometimes the best response.",
+        "Breathe. Reset. Move on."
+    ],
+
+    "fear": [
+        "Fear is temporary, courage lasts forever.",
+        "Do one thing today that scares you.",
+        "Confidence grows with action.",
+        "You are capable of more than you know."
+    ],
+
+    "neutral": [
+        "Stay focused and keep improving.",
+        "Consistency creates success.",
+        "Small steps lead to big achievements.",
+        "Discipline beats motivation."
+    ],
+
+    "surprise": [
+        "Life is full of beautiful surprises.",
+        "Growth begins outside comfort zones."
+    ],
+
+    "disgust": [
+        "Let go of what drains your energy.",
+        "Protect your peace."
+    ]
+}
